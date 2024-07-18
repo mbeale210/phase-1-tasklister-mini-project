7f0a9c9c8c855a67bdd78e1fa77b06303cb50cf9
Mark Beale <bealebeaverst@gmail.com>
2024-07-18 15:54:52 -0700

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.create-task-form);
+  })
   // your code here
 });
